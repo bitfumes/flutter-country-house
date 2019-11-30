4bc0dc3d66759c2a89f1bd389cf3f127cb8743b3
sarthaksavvy <bitfumes@gmail.com>
2019-11-30 20:58:18 +0530

    showing svg image

--- lib/Screens/Country.dart
+++ lib/Screens/Country.dart
@@ -1,4 +1,6 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Country extends StatelessWidget {
   final Map country;
@@ -6,11 +8,96 @@ class Country extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(country);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.pink,
         title: Text(country['name']),
       ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: GridView(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          children: <Widget>[
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Capital'),
+              back: CountryDetailCard(
+                title: country['capital'],
+                color: Colors.deepOrange,
+              ),
+            ),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Population'),
+              back: CountryDetailCard(
+                title: country['population'].toString(),
+                color: Colors.deepPurple,
+              ),
+            ),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Flag'),
+              back: Card(
+                color: Colors.white,
+                elevation: 10,
+                child: Center(
+                    child: SvgPicture.network(
+                  country['flag'],
+                  width: 200,
+                )),
+              ),
+            ),
+            FlipCard(
+              direction: FlipDirection.VERTICAL,
+              front: CountryCard(title: 'Currency'),
+              back: CountryDetailCard(
+                title: country['currencies'][0]['name'],
+                color: Colors.blue,
+              ),
+            ),
+            CountryCard(title: 'Show on Map'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CountryDetailCard extends StatelessWidget {
+  final String title;
+  final MaterialColor color;
+  CountryDetailCard({this.title, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: color,
+      elevation: 10,
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
+
+class CountryCard extends StatelessWidget {
+  final String title;
+  const CountryCard({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Center(
+          child: Text(
+        title,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      )),
     );
   }
 }
